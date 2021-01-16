42cdbf2407b6d1816d8b3248b749492b01763773
Enchan <enchant0130@gmail.com>
2021-01-16 10:29:54 +0900

    [Fix]  QR Code generate

--- QRCodeGenerateEx/ViewController.swift
+++ QRCodeGenerateEx/ViewController.swift
@@ -14,18 +14,24 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let message = "Hello, World!!"
+
+        let context = CIContext()
+
+        // フィルタ設定
+        let message = "Okaaaay, Fantastiiiic"
         let descriptor = CIQRCodeDescriptor(payload: message.data(using: .utf8)!, symbolVersion: 1, maskPattern: 0, errorCorrectionLevel: .levelM)!
         
-        BarcodeView(frame: CGRect(x: 0, y: 0, width: 0, height: 0)).generateCodeFromDescriptor(descriptor: descriptor)
+        guard let filter = CIFilter(name: "CIBarcodeGenerator", parameters: ["inputBarcodeDescriptor": descriptor]) else {return}
         
-        barcodeView.descriptor = descriptor
-        //
-        //        let bView = BarcodeView(descriptor: descriptor)
-        //        self.view.addSubview(bView)
+        // フィルタ済みの画像を取得して
+        guard let output = filter.outputImage else {return}
+        guard let cgImage = context.createCGImage(output, from: output.extent) else {return}
+ 
+        // UIIに出力
+        let uiImage = UIImage(cgImage: cgImage)
+        let imageView = UIImageView(image: uiImage)
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = self.view.frame
+        self.view.addSubview(imageView)
     }
-    
-    
 }
-
